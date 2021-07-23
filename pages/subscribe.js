255d1d064754ef1e68c694a07dfacccb892b7392
enriquepablo <enrique@cazalla.net>
2021-07-23 12:59:51 +0200

    some style for the subscription page

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -66,47 +66,54 @@ class NewsletterSubscription extends Component {
         return (
             <Layout {...this.props}>
 
-              <h1>Prenumerera på vårt nyhetsbrev</h1>
-              <h2>Håll dig uppdaterad om vad som händer inom Sunet. Fyll i din e-postadress nedan för att börja prenumerera.</h2>
-              {!this.state.acceptedConds && (
-                <>
-                  <br key="0" />
-                  <span key="1">XXX successfullySubscribed</span>
-                </>
-              )}
-              <p>Du behöver också ta del av informationen om behandling av personuppgifter och samtycka till att Vetenskapsrådet, där Sunet är en avdelning, behandlar dina personuppgifter.</p>
-              <p>E-POST (OBLIGATORISK):</p>
-              <form id="newsletter-form">
-                <p>
-                  <input type="email" name="email" id="email-input" />
-                  {!this.state.emailEntered && (
-                    <>
-                      <br key="0" />
-                      <span key="1">XXX this is required</span>
-                    </>
-                  )}
-                </p>
-                <p>
-                  <input type="checkbox" name="conditions" id="conditions-input" />
-                  Jag har läst <a href="https://sunet.se/om-sunet/information-om-behandling-av-personuppgifter/">informationen</a> och samtycker till Vetenskapsrådets behandling av mina personuppgifter.
-                  {!this.state.acceptedConds && (
-                    <>
-                      <br key="0" />
-                      <span key="1">XXX this is required</span>
-                    </>
-                  )}
-                </p>
-                <script src="https://www.google.com/recaptcha/api.js" async defer />
-                <div>
-                  <Recaptcha
-                    sitekey={config.recaptchaKey}
-                    verifyCallback={this.verification}
-                    type="checkbox"
-                  />
+              <div className="container subscription-page">
+                <div className="row single m-80">
+                  <div className="col-lg-10">
+
+                    <h1>Prenumerera på vårt nyhetsbrev</h1>
+                    <h2>Håll dig uppdaterad om vad som händer inom Sunet. Fyll i din e-postadress nedan för att börja prenumerera.</h2>
+                    {!this.state.acceptedConds && (
+                      <>
+                        <br key="0" />
+                        <span key="1">XXX successfullySubscribed</span>
+                      </>
+                    )}
+                    <p>Du behöver också ta del av informationen om behandling av personuppgifter och samtycka till att Vetenskapsrådet, där Sunet är en avdelning, behandlar dina personuppgifter.</p>
+                    <p className="font-weight-bold">E-POST (OBLIGATORISK):</p>
+                    <form id="newsletter-form">
+                      <p>
+                        <input type="email" name="email" id="email-input" />
+                        {!this.state.emailEntered && (
+                          <>
+                            <br key="0" />
+                            <span key="1">XXX this is required</span>
+                          </>
+                        )}
+                      </p>
+                      <p>
+                        <input type="checkbox" name="conditions" id="conditions-input" />
+                        Jag har läst <a href="https://sunet.se/om-sunet/information-om-behandling-av-personuppgifter/">informationen</a> och samtycker till Vetenskapsrådets behandling av mina personuppgifter.
+                        {!this.state.acceptedConds && (
+                          <>
+                            <br key="0" />
+                            <span key="1">XXX this is required</span>
+                          </>
+                        )}
+                      </p>
+                      <script src="https://www.google.com/recaptcha/api.js" async defer />
+                      <div>
+                        <Recaptcha
+                          sitekey={config.recaptchaKey}
+                          verifyCallback={this.verification}
+                          type="checkbox"
+                        />
+                      </div>
+                      <button id="submit-button" className="font-weight-bold" onClick={this.subscribe.bind(this)}>PRENUMERERA</button>
+                    </form>
+                    <p>Vill du avsluta din prenumeration? Maila till <a href="mailto:info@sunet.se">info@sunet.se</a> så tar vi bort dig från listan.</p>
+                  </div>
                 </div>
-                <button id="submit-button" onClick={this.subscribe.bind(this)}>PRENUMERERA</button>
-              </form>
-              <p>Vill du avsluta din prenumeration? Maila till info@sunet.se så tar vi bort dig från listan.</p>
+              </div>
             </Layout>
         );
     }
