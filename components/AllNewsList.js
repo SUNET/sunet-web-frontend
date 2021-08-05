a07b85a5518e8a4a4cd5d1d86e2e6d47fdeb6c1e
enriquepablo <enrique@cazalla.net>
2021-08-05 14:52:50 +0200

    localize date

--- components/AllNewsList.js
+++ components/AllNewsList.js
@@ -14,13 +14,18 @@ const routes = {
 const AllNewsList = ({ news, locale }) => {
 	
 	function renderNewsroom() {
+
+
 		return news.map((item) => {
+      const date = new Date(item.date);
+      const options = { day: 'numeric', month: 'long', year: 'numeric' };
+      const localizedDate = date.toLocaleDateString(locale, options);
 			return (
 				<Link href={`${routes[locale.lang]}/${item.slug}`} as={`${routes[locale.lang]}/${item.slug}`} key={item.id}>
 					<a>
             <div className="card">
               <div className="card-tags">
-                <span>{item.date}</span>
+                <span>{localizedDate}</span>
               </div>
               <div className="card-content">
                 <div className="header-container">
