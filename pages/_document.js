991a0e641eb4cfad79203a92e63a501bc4ebfb7b
Erik Bergström <berra@sunet.se>
2021-10-25 08:09:01 +0200

    removed matomo

--- pages/_document.js
+++ pages/_document.js
@@ -4,22 +4,7 @@ export default class CustomDocument extends Document {
     render() {
         return (
             <html lang="sv">
-                <Head>
-                  <script dangerouslySetInnerHTML={{ __html: `
-                    var _paq = window._paq = window._paq || [];
-                    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-                    _paq.push(['trackPageView']);
-                    _paq.push(['enableLinkTracking']);
-                    (function() {
-                      var u="https://analytics.sunet.se/";
-                      _paq.push(['setTrackerUrl', u+'matomo.php']);
-                      _paq.push(['setSiteId', '3']);
-                      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                      g.type='text/javascript'; g.async=true; g.src=u+'matomo.js';
-                      s.parentNode.insertBefore(g,s);
-                    })();
-                  ` }} />
-                </Head>
+                <Head></Head>
                 <body>
                     <Main />
                     <NextScript />
