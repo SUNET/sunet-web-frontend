748f478913fa41ede104d5569964125ac6da0433
enriquepablo <enrique@cazalla.net>
2022-04-13 17:34:01 +0200

    security.txt page

---
+++ pages/security.txt.js
@@ -0,0 +1,24 @@
+
+import { GetServerSideProps } from 'next';
+import React from 'react';
+
+const SecurityTxt = () => null
+
+export const getServerSideProps = async ({ res }) => {
+  const date = new Date();
+  date.setMonth(date.getMonth() + 2);
+  if (res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.write(`Contact: https://www.sunet.se/om-sunet/sakerhet\n`);
+    res.write(`Contact: mailto:abuse@sunet.se\n`);
+    res.write(`Expires: ${date.toISOString()}\n`);
+    res.write(`Preferred-Languages: en, sv\n`);
+    res.write(`Acknowledgments: https://www.sunet.se/om-sunet/sakerhet/hall-of-fame\n`);
+    res.write(`Encryption: https://keys.openpgp.org/vks/v1/by-fingerprint/9CBFAF18D1E1AF3176E74C3E2F23CB193ACFD1F0`);
+    res.end();
+  }
+	  return {props:{}};
+}
+
+export default SecurityTxt
+
