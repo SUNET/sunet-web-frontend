e3fb9f51b3e131ef847ea00b12d8e23a8a7c183b
Erik Bergström <berra@sunet.se>
2022-09-06 08:26:56 +0200

    updated subscribe.js

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -68,38 +68,7 @@ class NewsletterSubscription extends Component {
                     <p>Du behöver också ta del av informationen om behandling av personuppgifter och samtycka till att Vetenskapsrådet, där Sunet är en avdelning, behandlar dina personuppgifter.</p>
                     <p className="font-weight-bold">E-POST (OBLIGATORISK):</p>
 
-                    <form name="SubscribeForm" onSubmit={() => {return validateForm()}} action="https://public.paloma.se/subscription/register" method="POST">
-                      <input type="hidden" name="FormKey" value="c4550aa5-19dd-4144-8c8d-7d3d0ce70735" />
-                      <input type="hidden" name="Language" value="sv" />
-                      <input type="hidden" name="LegalBasis" value="1" />
-                      <input type="hidden" name="TermsGuid" value="e564ccce-9542-483c-bc94-046ef1777061" />
-                      <input type="hidden" name="Lists" value="2118fa62-01b2-4d36-92e3-1b8cfd471382" />
-                      <p>
-                        <input type="email" id="SubscriberForm_email" name="email" required placeholder="namn.namnsson@email.com" />
-                      </p>
-                      <span id="SubscriberForm_email_error_required">Obligatorisk</span>
-                      <span id="SubscriberForm_email_error_0">Ogiltig e-postadress</span>
-                      <span id="SubscriberForm_email_error_1">Max 400 tecken</span>
-                      <input type="hidden" name="OptInSenderName" value="SUNET" />
-                      <input type="hidden" name="OptInSenderEmail" value="noreply@nyhetsbrev.sunet.se" />
-                      <input type="hidden" name="OptInMessage" value="PHA+SGVqITxzdHJvbmc+IDwvc3Ryb25nPjwvcD4KCjxwPjxzdHJvbmc+VGFjayBmJm91bWw7ciBhdHQgZHUgdmFsdCBhdHQgcHJlbnVtZXJlcmEgcCZhcmluZzsgU3VuZXRzIG55aGV0c2JyZXYhJm5ic3A7PC9zdHJvbmc+PC9wPgoKPHA+QmVrciZhdW1sO2Z0YSBkaW4gcHJlbnVtZXJhdGlvbiBnZW5vbSBhdHQga2xpY2thIHAmYXJpbmc7IGwmYXVtbDtua2VuLiZuYnNwOzwvcD4KCjxwPlYmYXVtbDtsa29tbWVuISZuYnNwOzwvcD4KCjxwPiZuYnNwOzwvcD4KCjxwPlBTOiBEdSBrYW4gbiZhdW1sO3Igc29tIGhlbHN0IGF2c2x1dGEgZGluIHByZW51bWVyYXRpb24gZ2Vub20gYXR0IGtsaWNrYSBwJmFyaW5nOyAmcXVvdDthdnByZW51bWVyZXJhJnF1b3Q7IGkgZXR0IG55aGV0c2JyZXYgZWxsZXIga29udGFrdGEgb3NzIHAmYXJpbmc7IGluZm9Ac3VuZXQuc2U8L3A+Cg==" />
-                      <input type="hidden" name="ThanksPage" value="https://sunet.se/om-sunet/nyhetsbrev-tack" />
-                      <input type="hidden" name="CheckThanksPageAvailability" value="0" />
-			<p>
-			    <input required type="checkbox" id="accept-conditions" name="haschecked" /> 
-          &nbsp;  Jag har läst &nbsp; 
-                        <a href="/om-sunet/nyhetsbrev-personuppgiftsbehandling">
-                          informationen
-                        </a>
-                            &nbsp;
-			    och samtycker till Vetenskapsrådets behandling av mina personuppgifter.
-                      
-                      </p>
-                      <input type="submit" id="submit-button" value="Skicka"/>
-                    </form>
-
-
-	      <form name="SubscribeForm" method="post" action="https://ui.ungpd.com/Api/Subscriptions/723e8a84-1f5c-4c1e-8837-b4755f4e6244">
+	      <form name="SubscribeForm" method="post" onSubmit={() => {return validateForm()}} action="https://ui.ungpd.com/Api/Subscriptions/723e8a84-1f5c-4c1e-8837-b4755f4e6244">
 		  <input type="hidden" name="ListIds" value="a7fb77c1-f7a8-48a7-8756-ea306f1dd3f6" id="listIda7fb77c1-f7a8-48a7-8756-ea306f1dd3f6" />
 		  <input type="hidden" name="SubscriptionConfirmedUrl" value="https://www.sunet.se/om-sunet/valkommen-som-prenumerant/" />
                   <input type="hidden" name="SubscriptionFailedUrl" value="https://www.sunet.se/om-sunet/nagot-gick-fel" />
@@ -108,23 +77,19 @@ class NewsletterSubscription extends Component {
 		  <p>
                   <input type="email" id="SubscriberForm_email" name="Contact[Email]"  required placeholder="namn.namnsson@email.com" /><br />
 		      </p>
-
+                      <span id="SubscriberForm_email_error_required">Obligatorisk</span>
+                      <span id="SubscriberForm_email_error_0">Ogiltig e-postadress</span>
+                      <span id="SubscriberForm_email_error_1">Max 400 tecken</span>
 		  <p><label for="contactConsentText"> 
 		      <input type="checkbox" required name="ConsentText" id="contactConsentText" value="Jag vill få relevant information från Sunet till min inkorg. Sunet ska inte dela eller sälja min pe\
 rsonliga information. Jag kan när som helst avsluta prenumerationen." />
 
-&nbsp;  Jag har läst &nbsp;                                                                                                                                                                                                   
-                          <a href="/om-sunet/nyhetsbrev-personuppgiftsbehandling">                                                                                                                                                                                  informationen                                                                                                                                                                                                                        </a>                                                                                                                                                                                                                                       &nbsp;                                                                                                                                                                                                                                 och samtycker till Vetenskapsrådets behandling av mina personuppgifter.
-			  </label>
-		      </p>
+&nbsp;  Jag har läst &nbsp; <a href="/om-sunet/nyhetsbrev-personuppgiftsbehandling">informationen</a>&nbsp; och samtycker till Vetenskapsrådets behandling av mina personuppgifter.</label>
+ </p>
 
               <input type="submit" id="submit-button" value="Skicka" />
 </form>
-
-
-			
-                    <p>Vill du avsluta din prenumeration? Klicka på länken “Avprenumerera” längst ner i nyhetsbrevet.</p>
-            
+                    <p>Vill du avsluta din prenumeration? Klicka på länken “Avprenumerera” längst ner i nyhetsbrevet.</p>            
                      </div>
                   </article>
                   </main>
