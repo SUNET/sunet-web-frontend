5739338a327c89b6a07371b4fa3bef81e4276f4f
enriquepablo <enrique@cazalla.net>
2022-10-20 12:11:11 +0200

    jira tickets page, WIP

--- pages/ticket.js
+++ pages/ticket.js
@@ -75,9 +75,7 @@ class Ticket extends Component {
 						<article className="col-lg-8 offset-lg-2">
               <h1 id="main-title">{ticket.key}: [{ticket.fields.project.name}] {title}</h1>
               <p className="ticket-creation">
-                Created by 
-                <span	dangerouslySetInnerHTML={{ __html: `${ticket.fields.creator.displayName} &lt;${ticket.fields.creator.emailAddress}&gt;` }} />
-                on {new Date(ticket.fields.created).toLocaleString()}
+                <span	dangerouslySetInnerHTML={{ __html: `Created by ${ticket.fields.creator.displayName} &lt;${ticket.fields.creator.emailAddress}&gt; on ${new Date(ticket.fields.created).toLocaleString()}` }} />
               </p>
               <p className="ticket-description">{ticket.fields.description}</p>
               <dl>
