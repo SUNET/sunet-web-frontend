6bea695465d4982da9ee58ed8b4949e8b725449d
enriquepablo <enrique@cazalla.net>
2022-10-20 15:00:15 +0200

    jira tickets page, WIP

--- pages/ticket.js
+++ pages/ticket.js
@@ -45,19 +45,73 @@ class Ticket extends Component {
     for (const fname in ticket.fields) {
       if (! usedNames.includes(fname)) {
         const val = ticket.fields[fname];
-        if (val) {
+        if (val && !(typeof(val) === 'object' && Object.keys(val).length === 0)) {
           const name = this.props.customFields.find(f => f.id === fname).name;
-          if (typeof(val) === 'string') {
-            kvpairs.push([name, val]);
-          } else if (val.hasOwnProperty('name')) {
-            kvpairs.push([name, val.name]);
-          } else {
-            kvpairs.push([name, `${val}`]);
-          }
+          const val = renderCustomField(fname, val);
+          kvpairs.push([name, val]);
         }
       }
     };
     return kvpairs;
+  }
+
+  renderUser(val) {
+    return `${val.displayName} &lt;${val.emailAddress}&gt;`
+  }
+
+  renderCustomField(id, val) {
+    const field = this.props.customFields.find(f => f.id === id);
+    switch (field.schema.type) {
+      case 'array':
+        switch (field.schema.items) {
+          case 'string':
+            return val.map((item, i) => (<span key={i}>{item}</span>)).join(',&nbsp');
+          case 'user':
+            return val.map((item, i) => (<span key={i}>{this.renderUser(item)}</span>)).join(',&nbsp');
+          case 'option':
+            return val.map((item, i) => (<span key={i}>{item.value}</span>)).join(',&nbsp');
+
+          case 'version':
+          case 'issuelinks':
+          case 'component':
+          case 'sd-customerorganization':
+          case 'worklog':
+          case 'attachment':
+
+          default:
+            return val.map((item, i) => (<span key={i}>{`${item}`}</span>)).join(',&nbsp');
+        }
+      case 'string':
+      case 'number':
+        return (<span>val</span>);
+      case 'date':
+      case 'datetime':
+        return (<span>{new Date(val).toLocaleString()}</span>);
+      case 'user':
+        return (<span>{this.renderUser(val)}</span>);
+      case 'option':
+        return (<span>{val.value}</span>);
+
+      case 'priority':
+      case 'any':
+      case 'status':
+      case 'progress':
+      case 'sd-approvals':
+      case 'votes':
+      case 'issuetype':
+      case 'project':
+      case 'resolution':
+      case 'sd-customerrequesttype':
+      case 'sd-servicelevelagreement':
+      case 'watches':
+      case 'timetracking':
+      case 'securitylevel':
+      case 'comments-page':
+
+      case 'option-with-child':
+      default:
+        return `${val}`;
+    }
   }
 
 	render() {
@@ -65,6 +119,7 @@ class Ticket extends Component {
 		if (error) return <Error statusCode={404} />;
 
     const customFields = this.getCustomFields(ticket);
+    const creation = `Created by ${this.renderUser(ticket.fields.creator)} on ${new Date(ticket.fields.created).toLocaleString()}`;
 
 		return (
 			<Layout {...this.props}>
@@ -75,7 +130,7 @@ class Ticket extends Component {
 						<article className="col-lg-8 offset-lg-2">
               <h1 id="main-title">{ticket.key}: [{ticket.fields.project.name}] {title}</h1>
               <p className="ticket-creation">
-                <span	dangerouslySetInnerHTML={{ __html: `Created by ${ticket.fields.creator.displayName} &lt;${ticket.fields.creator.emailAddress}&gt; on ${new Date(ticket.fields.created).toLocaleString()}` }} />
+                <span	dangerouslySetInnerHTML={{ __html: creation }} />
               </p>
               <p className="ticket-description">{ticket.fields.description}</p>
               <dl>
