cc51fae47c1fa2403b2b39ba4e35820939e4e766
enriquepablo <enrique@cazalla.net>
2024-05-17 11:21:28 +0200

    working on the projekt section

--- components/ProjektLink.js
+++ components/ProjektLink.js
@@ -1,12 +1,15 @@
 import Link from 'next/link';
 import withLocale from './withLocale';
 import postTypes from '../post-types.json';
+import { getPersoner } from "../src/utils/index.js";
 
 const ProjektLink = ({ proj, locale }) => {
 	function getSlug() {
 		const slug = postTypes.find((item) => item.name === 'projekt').routes[locale.lang];
 		return `/${locale.slug}${slug}`;
 	}
+
+	    const persons = await getPersoner(lang);
 
 		let personId = -1;
 		if (proj && proj.acf && typeof proj.acf.person[0] !== 'undefined'
